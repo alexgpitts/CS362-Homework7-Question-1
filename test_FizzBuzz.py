8cc04de9161cf19bffdd8359e4b3f199509d1c66
alexgpitts <alexgpitts@gmail.com>
2021-03-03 19:59:07 -0800

    Wrote third test
    This test checks if numbers only divisible by 3 and not will return "Buzz"

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -13,6 +13,10 @@ class TestCase(unittest.TestCase):
     def test_FizzBuzz_2(self): 
         self.assertEqual(FizzBuzz.FizzBuzz(12), "Fizz") 
 
+    #A test that checks a number divisible by 5 and not 3
+    def test_FizzBuzz_3(self): 
+        self.assertEqual(FizzBuzz.FizzBuzz(20), "Buzz") 
+
 
 
 if __name__ == '__main__':
